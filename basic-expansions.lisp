1aadb0679b1804bdf84aac33c38a821c2702d26c
Carlo Sganzerla <carlosganzrl@hotmail.com>
2021-09-28 18:47:40 -0300

    did stuff

--- basic-expansions.lisp
+++ basic-expansions.lisp
@@ -12,30 +12,8 @@
 (dolist (e (list 5 4 3 2 1))
   (print e))
 
-
-
 ;; Creating our own macros
 
-(at-least-2 1 2 3 (print "hello good old friend!"))
-
-(at-least-2 1 nil nil (print "hello good old friend!"))
-
-(at-least-2-fn 1 2 3 (print "hello good old friend!"))
-
-(or (and 1 2)
-    (and 1 3)
-    (and 2 3))
-
-(dolist (e1 lst)
-  (dolist (e2 (cdr lst))
-    (setf res (cons `(and ,e1 ,e2) res))
-    (setf lst (cdr lst))))
-`(or ,@res)  
-
-(defmacro at-least-2 (form1 form2 &rest forms)
-  (let ((lst (append (list form1 form2) forms)))
-    `(or ,@(combine-2 lst (lambda (e1 e2)
-                            `(and ,e1 ,e2))))))
 
 (defun combine-2 (lst fn)
   (let ((result nil))
@@ -45,8 +23,28 @@
       (dolist (i (cdr iter))
         (setf result (cons (funcall fn head i) result))))))
 
+(defmacro at-least-2 (form1 form2 &rest forms)
+  (let ((lst (append (list form1 form2) forms)))
+    `(or ,@(combine-2 lst (lambda (e1 e2)
+                            `(and ,e1 ,e2))))))
+
 (defun at-least-2-fn (form1 form2 &rest forms)
   (let ((lst (append (list form1 form2) forms)))
     (some #'identity (combine-2 lst (lambda (e1 e2)
                                       (and e1 e2))))))
+
+
+(or (and 1 2)
+    (and 1 3)
+    (and 1 4)
+    (and 2 3)
+    (and 2 4)
+    (and 3 4))
+
+(at-least-2 1 2 3 (print "hello good old friend!"))
+
+(at-least-2 1 nil nil (print "hello good old friend!"))
+
+(at-least-2-fn 1 2 3 (print "hello good old friend!"))
+
 
